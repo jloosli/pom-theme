7f7641badbc9e0d3a82923ed8aede13523858f0c
Jared Loosli <jloosli@gmail.com>
2015-02-12 17:34:04 -0700

    Updated to https

--- js/scripts.js
+++ js/scripts.js
@@ -27,4 +27,20 @@ jQuery( document ).ready(function( $ ) {
 		});
 	}
 	// [].forEach.call($('img'),function(img){var src=img.getAttribute('src');if(src.indexOf('/')===0){img.src='http://powerofmoms.com'+src;}})
+
+    // Add facebook conversion
+    (function(w) {
+        var _fbq = w._fbq || (w._fbq = []);
+        if (!_fbq.loaded) {
+            var fbds = document.createElement('script');
+            fbds.async = true;
+            fbds.src = '//connect.facebook.net/en_US/fbds.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(fbds, s);
+            _fbq.loaded = true;
+        }
+        _fbq.push(['addPixelId', '402087909938778']);
+    })(window);
+    window._fbq = window._fbq || [];
+    window._fbq.push(['track', 'PixelInitialized', {}]);
 });
